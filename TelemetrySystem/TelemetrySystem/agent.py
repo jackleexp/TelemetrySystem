858997e141df5fe8e11c757e0d61f76ee748b251
李杰 <jackleexp@gmail.com>
2018-05-23 19:09:00 +0800

    链接HopeRF盒子测试，基本同链接USRP一致

--- TelemetrySystem/TelemetrySystem/agent.py
+++ TelemetrySystem/TelemetrySystem/agent.py
@@ -3,8 +3,9 @@ import sys
 import serial
 import socket
 import time
+import datetime
 
-SERIALPORT = "COM1"
+SERIALPORT = "COM3"
 BAUDRATE = 9600
 parity = serial.PARITY_NONE
 
@@ -17,46 +18,48 @@ ser.parity = parity
 ser.timeout = None
 
 try:
-   ser.open()
+    ser.open()
 except serial.SerialException as e:
-   sys.stderr.write('Could not open serial port {}: {}\n'.format(ser.name, e))
-   sys.exit(1)
-   
+    sys.stderr.write('Could not open serial port {}: {}\n'.format(ser.name, e))
+    sys.exit(1)
 
 while True:
-	try:
-		sys.stderr.write("Opening connection to {}:{}...\n".format(server_ip, server_port))
-		client_socket = socket.socket()
-		try:
-			client_socket.connect((server_ip, server_port))
-		except socket.error as msg:
-			sys.stderr.write('WARNING: {}\n'.format(msg))
-			time.sleep(5)  # intentional delay on reconnection as client
-			continue
-		sys.stderr.write('Connected\n')
-		client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-		
-		#~ client_socket.settimeout(5)
-	except AttributeError:
-		pass # XXX not available on windows
+    try:
+        sys.stderr.write("Opening connection to {}:{}...\n".format(server_ip, server_port))
+        client_socket = socket.socket()
+        try:
+            client_socket.connect((server_ip, server_port))
+        except socket.error as msg:
+            sys.stderr.write('WARNING: {}\n'.format(msg))
+            time.sleep(5)  # intentional delay on reconnection as client
+            continue
+        sys.stderr.write('Connected\n')
+        client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        
+        #~ client_socket.settimeout(5)
+    except AttributeError:
+        pass # XXX not available on windows
 
 
-	try:
-		while True:
-			try:
-				data = ser.read(220)
-				if not data:
-					continue
-				sys.stderr.write("Recv one pack from serial\n")
-				client_socket.send(data)
-			except serial.SerialException as msg:
-				sys.stderr.write("ERROR: {} \n".format(msg))
+    try:
+        while True:
+            try:
+                data = ser.read(220)
+                if not data:
+                    continue
+                sys.stderr.write("{}:Recv one pack from serial\n".format(datetime.datetime.now()))
+                client_socket.send(data)
+            except serial.SerialException as msg:
+                sys.stderr.write("ERROR: {} \n".format(msg))
+            except KeyboardInterrupt:
+                sys.stderr.write("User abort\n")
+                break
 
-	except KeyboardInterrupt:
-		sys.stderr.write("User abort\n")
-	except socket.error as msg:
-		sys.stderr.write("ERROR: {}\n".format(msg))
-	finally:
-		sys.stderr.write("Disconnected\n")
-		client_socket.close()
-	
+    except KeyboardInterrupt:
+        sys.stderr.write("User abort\n")
+    except socket.error as msg:
+        sys.stderr.write("ERROR: {}\n".format(msg))
+    finally:
+        sys.stderr.write("Disconnected\n")
+        client_socket.close()
+    
